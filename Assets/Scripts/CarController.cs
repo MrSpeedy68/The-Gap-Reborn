cadcf22bf3411873cb73c69bf77d1f6761217b95
Adrian Hebel <47033681+MrSpeedy68@users.noreply.github.com>
2022-01-29 19:54:19 +0000

    Car Controller

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,90 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private const string HORIZONTAL = "Horizontal";
+    private const string VERTICAL = "Vertical";
+
+    private float horizontalInput;
+    private float verticleInput;
+    private float currentSteerAngle;
+    private float currentbreakForce;
+    private bool isBreaking;
+    
+    [SerializeField] private float motorForce;
+    [SerializeField] private float breakForce;
+    [SerializeField] private float maxSteerAngle;
+
+    [SerializeField] private WheelCollider frontLeftWC;
+    [SerializeField] private WheelCollider frontRightWC;
+    [SerializeField] private WheelCollider rearLeftWC;
+    [SerializeField] private WheelCollider rearRightWC;
+    
+    [SerializeField] private Transform frontLeftTransform;
+    [SerializeField] private Transform frontRightTransform;
+    [SerializeField] private Transform rearLeftTransform;
+    [SerializeField] private Transform rearRightTransform;
+    
+    private void FixedUpdate()
+    {
+        GetInput();
+        HandleMotor();
+        HandleSteering();
+        UpdateWheels();
+    }
+
+    private void HandleMotor()
+    {
+        frontLeftWC.motorTorque = verticleInput * motorForce;
+        frontRightWC.motorTorque = verticleInput * motorForce;
+
+        currentbreakForce = isBreaking ? breakForce : 0f;
+
+        if (isBreaking)
+        {
+            ApplyBreaking();
+        }
+    }
+
+    private void ApplyBreaking()
+    {
+        frontLeftWC.brakeTorque = currentbreakForce;
+        frontRightWC.brakeTorque = currentbreakForce;
+        rearLeftWC.brakeTorque = currentbreakForce;
+        rearRightWC.brakeTorque = currentbreakForce;
+    }
+
+    private void HandleSteering()
+    {
+        currentSteerAngle = maxSteerAngle * horizontalInput;
+        frontLeftWC.steerAngle = currentSteerAngle;
+        frontRightWC.steerAngle = currentSteerAngle;
+    }
+    
+    private void GetInput()
+    {
+        horizontalInput = Input.GetAxis(HORIZONTAL);
+        verticleInput = Input.GetAxis(VERTICAL);
+        isBreaking = Input.GetKey(KeyCode.Space);
+    }
+
+    private void UpdateWheels()
+    {
+        UpdateSingleWheel(frontLeftWC, frontLeftTransform);
+        UpdateSingleWheel(frontRightWC, frontRightTransform);
+        UpdateSingleWheel(rearLeftWC, rearLeftTransform);
+        UpdateSingleWheel(rearRightWC, rearRightTransform);
+    }
+
+    private void UpdateSingleWheel(WheelCollider wheelCollider, Transform wheelTransform)
+    {
+        Vector3 pos;
+        Quaternion rot;
+        wheelCollider.GetWorldPose(out pos, out rot);
+        wheelTransform.rotation = rot;
+        wheelTransform.position = pos;
+    }
+}
