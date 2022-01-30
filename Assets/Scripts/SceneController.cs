e88cc118528d2d1c1b2f4361bc488fa613e782d2
Jack Delahunt <20085409@mail.wit.ie>
2022-01-30 16:20:46 +0000

    Feat: added car select

--- Assets/Scripts/SceneController.cs
+++ Assets/Scripts/SceneController.cs
@@ -1,10 +1,39 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    [SerializeField] private GameObject CameroCar;
+    [SerializeField] private GameObject F1Car;
+     
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        SceneManager.sceneLoaded += OnSceneChange;
+    }
+
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneChange;
+    }
+
     public void LoadScene()
     {
-        SceneManager.LoadScene(PlayerPrefs.GetString("level") + "-" + PlayerPrefs.GetString("time"));
+        SceneManager.LoadScene(PlayerPrefs.GetString("level") + "-" + PlayerPrefs.GetString("time"), LoadSceneMode.Single);
+    }
+
+    private void OnSceneChange(Scene scene, LoadSceneMode mode)
+    {
+        if (scene.buildIndex != 0)
+        {
+            switch (PlayerPrefs.GetString("car"))
+            {
+                case "Camero":
+                    Instantiate(CameroCar); break;
+                case "F1":
+                    Instantiate(F1Car); break;
+            }
+        }
     }
 }
